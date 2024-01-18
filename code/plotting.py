15cffc6b9d794f9924073c129d5dc44b99c6c0bc
Michelle Wang <tomichellewang@gmail.com>
2024-01-18 16:41:44 -0500

    ensure at least 3 ticks for log scale

--- code/plotting.py
+++ code/plotting.py
@@ -1,10 +1,15 @@
 import seaborn as sns
 from matplotlib.ticker import FormatStrFormatter
 
-def plot_timeseries(data, x, y, **kwargs):
+def plot_timeseries(data, x, y, log_scale=True, **kwargs):
     ax = sns.lineplot(data=data, x=x, y=y, **kwargs)
-    ax.set_yscale('log')
-    ax.minorticks_off()
+    if log_scale:
+        ax.set_yscale('log')
+        ax.minorticks_off()
+        _, y_max = ax.get_ylim()
+        if y_max < 100:
+            ax.set_ylim(top=100)
+            
     ax.get_yaxis().set_major_formatter(FormatStrFormatter('%.0f'))
     ax.set_ylim(bottom=1)
     ax.set_xlim(left=min(data[x]), right=max(data[x]))
